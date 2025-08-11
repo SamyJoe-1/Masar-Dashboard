2a9c48ab259d050d38a2f8c432265ee1f83a1634
SamyJoe <SamyJoe>
2025-08-11 18:01:50 +0300

    CRM

--- resources/views/livewire/jobs/index.blade.php
+++ resources/views/livewire/jobs/index.blade.php
@@ -17,58 +17,60 @@
             </button>
         </div>
     </div>
-    <x-table.default>
-        <thead>
-        <tr>
-            <x-table.thCheckbox></x-table.thCheckbox>
-            @foreach($tableColumns as $title => $columnInfo)
-                <x-table.th :columns="$selectedColumns" :column="@$columnInfo['column']" :sorting="@$columnInfo['sorting']" :sort-by="$sortField" :sort-dir="$sortDirection">{{ $title }}</x-table.th>
-            @endforeach
-        </tr>
-        </thead>
-        <tbody>
-        @forelse($jobs as $job)
+    @if(count($jobs))
+        <x-table.default>
+            <thead>
             <tr>
-                <x-table.tdCheckbox :value="$job->id"></x-table.tdCheckbox>
-                <x-table.td :columns="$selectedColumns" column="id">{{ $job->id }}</x-table.td>
-                <x-table.td :columns="$selectedColumns" column="title">{{ $job->title }}</x-table.td>
-                <x-table.td :columns="$selectedColumns" column="description">
-                    {{ showLess($job->description, 30) }}
-                </x-table.td>
-                <x-table.td :columns="$selectedColumns" column="applicants_count">
-                    <span class="text-{{ $job->applicants_count ? "":"danger" }}">{{ $job->applicants_count }} متقدم</span>
-                </x-table.td>
-                <x-table.td :columns="$selectedColumns" column="approved_applicants_count" :danger="$job->approved_applicants_count">
-                    <span class="text-{{ $job->approved_applicants_count ? "success":"danger" }}">{{ $job->approved_applicants_count }} متقدم</span>
-                </x-table.td>
-                <x-table.td :columns="$selectedColumns" column="rejected_applicants_count" :danger="$job->approved_applicants_count">
-                    <span class="text-{{ $job->rejected_applicants_count ? "":"danger" }}">{{ $job->rejected_applicants_count }} متقدم</span>
-                </x-table.td>
-                <x-table.td :columns="$selectedColumns" column="close">
-                    <x-badge.active :status="!$job->close"></x-badge.active>
-                </x-table.td>
-                <x-table.td :columns="$selectedColumns" column="public">
-                    @if($job->public)
-                        <i class="bx bx-planet fs-5"></i>
-                    @else
-                        <i class="bx bx-lock fs-5"></i>
-                    @endif
-                </x-table.td>
-                <x-table.td :columns="$selectedColumns" column="created_at">{{ dateFormat_1($job->created_at) }}</x-table.td>
-                <x-table.td :columns="$selectedColumns" column="updated_at">{{ dateFormat_1($job->updated_at) }}</x-table.td>
-                <x-table.td :columns="$selectedColumns" column="action">
-                    <div class="d-flex card-actions">
-                        <a wire:click="Delete({{ $job->id }})" class="cursor-pointer"><i class='bx bx-trash text-danger'></i></a>
-                        <a href="{{ route('dashboard.hr.jobs.edit', $job) }}" wire:navigate class="ms-1"><i class='text-primary bx bx-edit'></i></a>
-                        <a href="{{ route('dashboard.hr.jobs.show', $job) }}" wire:navigate class="ms-1"><i class='text-success bx bx-show'></i></a>
-                    </div>
-                </x-table.td>
+                <x-table.thCheckbox></x-table.thCheckbox>
+                @foreach($tableColumns as $title => $columnInfo)
+                    <x-table.th :columns="$selectedColumns" :column="@$columnInfo['column']" :sorting="@$columnInfo['sorting']" :sort-by="$sortField" :sort-dir="$sortDirection">{{ $title }}</x-table.th>
+                @endforeach
             </tr>
-        @empty
-            <h1>{{ __("words.No records found.") }}</h1>
-        @endforelse
-        </tbody>
-    </x-table.default>
+            </thead>
+            <tbody>
+            @foreach($jobs as $job)
+                <tr>
+                    <x-table.tdCheckbox :value="$job->id"></x-table.tdCheckbox>
+                    <x-table.td :columns="$selectedColumns" column="id">{{ $job->id }}</x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="title">{{ $job->title }}</x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="description">
+                        {{ showLess($job->description, 30) }}
+                    </x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="applicants_count">
+                        <span class="text-{{ $job->applicants_count ? "":"danger" }}">{{ $job->applicants_count }} متقدم</span>
+                    </x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="approved_applicants_count" :danger="$job->approved_applicants_count">
+                        <span class="text-{{ $job->approved_applicants_count ? "success":"danger" }}">{{ $job->approved_applicants_count }} متقدم</span>
+                    </x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="rejected_applicants_count" :danger="$job->approved_applicants_count">
+                        <span class="text-{{ $job->rejected_applicants_count ? "":"danger" }}">{{ $job->rejected_applicants_count }} متقدم</span>
+                    </x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="close">
+                        <x-badge.active :status="!$job->close"></x-badge.active>
+                    </x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="public">
+                        @if($job->public)
+                            <i class="bx bx-planet fs-5"></i>
+                        @else
+                            <i class="bx bx-lock fs-5"></i>
+                        @endif
+                    </x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="created_at">{{ dateFormat_1($job->created_at) }}</x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="updated_at">{{ dateFormat_1($job->updated_at) }}</x-table.td>
+                    <x-table.td :columns="$selectedColumns" column="action">
+                        <div class="d-flex card-actions">
+                            <a wire:click="Delete({{ $job->id }})" class="cursor-pointer"><i class='bx bx-trash text-danger'></i></a>
+                            <a href="{{ route('dashboard.hr.jobs.edit', $job) }}" wire:navigate class="ms-1"><i class='text-primary bx bx-edit'></i></a>
+                            <a href="{{ route('dashboard.hr.jobs.show', $job) }}" wire:navigate class="ms-1"><i class='text-success bx bx-show'></i></a>
+                        </div>
+                    </x-table.td>
+                </tr>
+            @endforeach
+            </tbody>
+        </x-table.default>
+    @else
+        <h5 class="text-secondary" style="text-align: center">{{ __("words.No records found.") }}</h5>
+    @endif
     {{ $jobs->links() }}
 </div>
 
