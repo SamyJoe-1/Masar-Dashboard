21c88bea377d8325032fd37ebe8f776e0a1e871b
SamyJoe <SamyJoe>
2025-08-14 12:38:25 +0300

    CRM

--- app/Console/Commands/MatchCVs.php
+++ app/Console/Commands/MatchCVs.php
@@ -2,8 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Log\LogHelper;
+use App\Models\Applicant;
 use App\Models\JobApp;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class MatchCVs extends Command
 {
@@ -24,10 +27,56 @@ class MatchCVs extends Command
     /**
      * Execute the console command.
      */
+
     public function handle()
     {
-//        $application = JobApp::where('close', false)->whereHas('dwqdwq',  function (){
-//
-//        })->get();
+        try {
+            $applicants = Applicant::where('processing', true)
+                ->where('status', 'pending')
+                ->get();
+
+            if ($applicants->count()) {
+                $urls = [];
+                $jobDescription = $applicants[0]->job_app->description ?? '-';
+                foreach ($applicants as $applicant) {
+                    $urls[] = asset($applicant->file->fullpath);
+                    $http = Http::timeout(6000)->acceptJson();
+                }
+
+                $this->info($applicants->count() . ' Applicant(s) caught');
+
+                $response = $http->post(config('app.match_cv_url') . '/match-cvs-from-urls', [
+                    'job_description' => $jobDescription,
+                    'urls' => $urls,
+                    'output_format' => 'json',
+                    "debug" => 'true'
+                ]);
+
+                if ($response->successful() && empty($response->json()['errors'])) {
+                    $this->info('Response received. Updating applicants profiles...');
+                    foreach ($applicants as $key => $applicant) {
+                        $result = $response->json()['results'][$key] ?? [];
+                        $status = @$result['status'] == Applicant::APPROVAL_KEYS[1];
+                        $applicant->update([
+                            'information' => $result,
+                            'status' => $status ? 'waiting for answering' : 'rejected',
+                        ]);
+                        $this->info("Applicant ({$applicant->id}) updated successfully");
+                    }
+                    $this->info('Process completed successfully');
+                    return Command::SUCCESS;
+                } else {
+                    $this->error('API request failed: ' . $response->body());
+                    return Command::FAILURE;
+                }
+            } else {
+                $this->info('0 Applicants found');
+                return Command::SUCCESS;
+            }
+        } catch (\Exception $e) {
+            $this->error('Error occurred: ' . $e->getMessage());
+            LogHelper::logError($e);
+            return Command::FAILURE;
+        }
     }
 }
